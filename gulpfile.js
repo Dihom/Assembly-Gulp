da1d290cf65835799131f532ed93de6ee171739d
Juniorhamster <juniorham@yandex.ru>
2022-05-04 01:16:31 +0500

    realese

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,21 @@ const cleanCSS = require('gulp-clean-css')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
+const size = require('gulp-size')
+const newer = require('gulp-newer')
+const browsersync = require('browser-sync').create()
 const del = require('del')
 
 // Путь до объектов CSS и JS
 const paths = {
+    html: {
+        src: 'src/*.html',
+        dest: 'dist'
+    },
     styles: {
         src: 'src/styles/**/*.scss',
         dest: 'dist/css/'
@@ -17,46 +28,99 @@ const paths = {
     scripts: {
         src: 'src/scripts/**/*.js',
         dest: 'dist/js/'
+    },
+    images: {
+        src: 'src/img/**',
+        dest: 'dist/img'
     }
 }
 
 // Функция очистки файлов
 function clean() {
-    return del(['dist'])
+    return del(['dist/*', '!dist/img'])
+}
+
+function html() {
+    return gulp.src(paths.html.src)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(size({
+        showFiles: true
+    }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browsersync.stream())
 }
 
 // Компиляция scss в css
 function styles() {
     return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cleanCSS({
+        level: 2
+    }))
     .pipe(rename({
         basename: 'main',
         suffix: '.min'
     }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(size({
+        showFiles: true
+    }))
     .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browsersync.stream())
 }
 
 function scripts() {
-    return gulp.src(paths.scripts.src, {
-        sourcemaps: true
-    })
-    .pipe(babel())
+    return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(size({
+        showFiles: true
+    }))
     .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browsersync.stream())
+}
+
+function img () {
+    return gulp.src(paths.images.src)
+            .pipe(newer(paths.images.dest))
+		    .pipe(imagemin({
+                progressive: true
+            }))
+            .pipe(size({
+                showFiles: true
+            }))
+		    .pipe(gulp.dest(paths.images.dest))
 }
 
 // Отслеживание компиляции файлов(чтобы отключить Ctrl+C)
 function watch () {
+    browsersync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    })
+    gulp.watch(paths.html.dest).on('change', browsersync.reload)
+    gulp.watch(paths.html.src, html)
     gulp.watch(paths.styles.src, styles)
     gulp.watch(paths.scripts.src, scripts)
+    gulp.watch(paths.images.src, img)
 }
 
 // Последовательная серия выполнений функций
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
 
 exports.clean = clean
+exports.img = img
+exports.html = html
 exports.styles = styles
 exports.scripts =scripts
 exports.watch = watch
